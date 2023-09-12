1a4a68dcdc034256860d1081a8d273304af4b44d
fabris30 <mfabrisavila20@gmail.com>
2023-09-12 14:26:09 -0500

    cambio url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import Cookies from "js-cookie";
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + Cookies.get('token');
 const instance = axios.create({
-    baseURL: 'https://electoralapp.onrender.com/api/v1',
+ // baseURL: 'https://electoralapp.onrender.com/api/v1',
+ baseURL: 'https://electorappcontrol.vercel.app/api/v1',
+ 
   //  withCredentials: true,
 });
 export default instance;
